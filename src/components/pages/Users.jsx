aed86da0373c1ab2d35cf37d891755de7034038e
kaitosun <110577892+kaitosun@users.noreply.github.com>
2022-08-29 14:40:25 +0900

    Pageの途中

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザーの一覧です</h2>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
